4989f23603151e77415569b2b61eba46d17eef0f
John Monte <ph.johnmonte@yahoo.com>
2017-01-24 16:50:48 +0800

    dark to light on scroll

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
 new WOW().init();
 
 $('#type').typeIt({
-     speed: 100,
+     speed: 50,
      autoStart: false
+});
+
+var $corner = $(".border"),
+    $window = $(window);
+
+$window.scroll(function() {    
+    var scroll = $window.scrollTop();
+
+    if (scroll >= 400) {
+        $corner.addClass("light");
+    } else {
+        $corner.removeClass("light");
+    }
+});
+
+var $body = $("body"),
+    $window = $(window);
+
+$window.scroll(function() {    
+    var scroll = $window.scrollTop();
+
+    if (scroll >= 400) {
+        $body.addClass("light");
+    } else {
+        $body.removeClass("light");
+    }
 });
