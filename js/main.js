9fb1bf37e23b85a4f4410419286c1f85e9e74dbf
John Monte <ph.johnmonte@yahoo.com>
2018-12-12 17:43:14 +0800

    201812121743
    added hello languages

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ $window.scroll(function() {
 
 document.addEventListener('DOMContentLoaded', function(){
       Typed.new('.typed', {
-        strings: ["^500Hello^500", "^500Hola^500", "^500Nín hao^500", "^500Bonjour^500", "^500Ciao^500", "^500Kónnichi wa^500", "^500Annyeonghaseyo^500", "^500Buongiorno^500"],
+        strings: ["^500Hello^500", "^500Hola^500", "^500Nín hao^500", "^500As Salaam Alaikum^500", "^500Bonjour^500",  "^500Hallo^500", "^500Ciao^500", "^500Kónnichi wa^500", "^500Annyeonghaseyo^500", "^500Zdravstvuyte^500", "^500Namaste^500", "^500Kumusta^500"],
         typeSpeed: 40,
 		loop: true
       });
