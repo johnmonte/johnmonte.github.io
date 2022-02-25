0b559c88ef0e854ce104454a5d3953053122d368
John Monte <ph.johnmonte@yahoo.com>
2022-02-25 23:43:54 +0800

    202202251143
    updated hellos, work 01 year

--- js/main.js
+++ js/main.js
@@ -46,7 +46,7 @@ $window.scroll(function() {
 
 document.addEventListener('DOMContentLoaded', function(){
       Typed.new('.typed', {
-        strings: ["^500Hello^500", "^500Hola^500", "^500Nín hao^500", "^500As Salaam Alaikum^500", "^500Bonjour^500",  "^500Hallo^500", "^500Ciao^500", "^500Kónnichi wa^500", "^500Annyeonghaseyo^500", "^500Zdravstvuyte^500", "^500Namaste^500", "^500Kumusta^500"],
+        strings: ["^500Hello^500", "^500Hola^500", "^500你好^500", "^500ٱلسَّلَامُ عَلَيْكُمْ^500", "^500Bonjour^500",  "^500Hallo^500", "^500Ciao^500", "^500こんにちは^500", "^500안녕하세요^500", "^500Здравствуйте^500", "^500नमस्ते^500", "^500Kumusta^500"],
         typeSpeed: 40,
 		loop: true
       });
