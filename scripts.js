bc22596c2d4020c5e9c648cd0c023833de3375b4
John Monte <ph.johnmonte@yahoo.com>
2024-01-13 14:27:01 +0800

    202401131426

--- scripts.js
+++ scripts.js
@@ -20,7 +20,7 @@ buttons.forEach(btn => {
 });
 
 const toggle = document.getElementsByClassName('toggle-switch')[0];
-toggle.checked = false;
+toggle.checked = true;
 toggle.addEventListener("click", () => {
   if (toggle.checked === true) {
     theme.classList.add("dark");
